ac4abd38481ce447609158a3dca3434eb71edba1
Jamie Wilkinson <jamie@jamiedubs.com>
2022-03-09 15:10:19 -0500

    Implement many things, especially tests

--- ethereum/src/WebRing.sol
+++ ethereum/src/WebRing.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.10;
 
 contract WebRing {
@@ -14,10 +14,14 @@ contract WebRing {
   mapping (address => uint256) public owners;
   uint256 public membersCount;
 
-  event JoinWebRing (address who, uint256 id, address contractAddr, uint256 tokenId);
-  event LeaveWebRing (address who);
+  event JoinWebRing (address indexed who, uint256 id, address contractAddr, uint256 tokenId);
+  event LeaveWebRing (address indexed who);
 
   function join(address contractAddr, uint256 tokenId) public {
+    require(contractAddr != address(0), "can't add null address");
+    // tokenId sometimes 0 because blank, sometimes because programmers
+    require(indexOf(msg.sender) == 0, "you're already a member of the webring");
+
     uint256 nextId = membersCount + 1;
 
     members[nextId] = Member(
@@ -40,21 +44,34 @@ contract WebRing {
   }
 
   function leave() public {
+    require(indexOf(msg.sender) != 0, "you are not a member of the webring");
+
     uint256 id = owners[msg.sender];
     delete members[id];
     delete owners[msg.sender];
+    membersCount--;
     emit LeaveWebRing(msg.sender);
   }
 
-  function idFor(address who) public view returns (uint256) {
-    require(who != address(0));
+  function indexOf(address who) public view returns (uint256) {
     return owners[who];
   }
 
-  // function siteFor(address who) public returns (address, uint256) {
-  function siteFor(address who) public view returns (address) {
-    Member memory member = members[idFor(who)];
-    return member.contractAddr;
+  function myIndex() public view returns (uint256) {
+    return owners[msg.sender];
+  }
+
+  function memberInfo(address who) public returns (uint256, address, uint256) {
+    Member memory member = members[indexOf(who)];
+    return (member.id, member.contractAddr, member.tokenId);
+  }
+
+  function myInfo() public returns (uint256, address, uint256) {
+    return memberInfo(msg.sender);
+  }
+
+  function contractAddressFor(address who) public view returns (address) {
+    return members[indexOf(who)].contractAddr;
   }
 
 }
--- ethereum/src/test/WebRing.t.sol
+++ ethereum/src/test/WebRing.t.sol
@@ -4,25 +4,92 @@ pragma solidity 0.8.10;
 import "ds-test/test.sol";
 import "../WebRing.sol";
 import {Utilities} from "./utils/Utilities.sol";
-import {console} from "./utils/Console.sol";
+import {console} from "./utils/Console.sol"; // FIXME this is in forge-std too
 import {Vm} from "forge-std/Vm.sol";
 
 
 contract WebRingTest is DSTest {
+    Vm public constant vm = Vm(HEVM_ADDRESS);
+    // CheatCodes cheats = CheatCodes(HEVM_ADDRESS);
 
     WebRing webring;
+
+    address validMemberAddress = 0x3769c5700Da07Fe5b8eee86be97e061F961Ae340; // gawds
+    uint256 validMemberTokenId = 4569; // dark lord of the wood gang
+
+    address forgeDeployer = 0xb4c79daB8f259C7Aee6E5b2Aa729821864227e84;
 
     function setUp() public {
         webring = new WebRing();
     }
 
-    function testWebRing() public {
-        // assertTrue(true);
-        assertEq(webring.membersCount(), 0);
-        webring.join(
-            address(0),
-            0
-        );
-        assertEq(webring.membersCount(), 1);
+    // helpers
+    function validMemberJoin() private {
+        webring.join(validMemberAddress, validMemberTokenId);
     }
+
+    // tests
+    function testConsoleLog() public {
+      console.log("console.log() works in tests");
+    }
+
+    function testJoinAndLeave() public {
+        assertEq(webring.membersCount(), 0);
+
+        validMemberJoin();
+        assertEq(webring.membersCount(), 1);
+
+        webring.leave();
+        assertEq(webring.membersCount(), 0);
+    }
+
+    function testJoinEmitsEvent() public {
+      // FIXME not quite working
+      // vm.expectEmit(true, false, false, true);
+      // emit WebRing.JoinWebRing(validMemberAddress);
+      // validMemberJoin();
+    }
+
+    function testLeaveEmitsEvent() public {
+      // TODO
+    }
+
+    function testFailNullCannotJoin() public {
+      webring.join(address(0), 0);
+    }
+
+    function testNullTokenIdIsOK() public {
+      webring.join(validMemberAddress, 0);
+      assertEq(webring.membersCount(), 1);
+    }
+
+    function testIndexOf() public {
+      validMemberJoin();
+      assertEq(
+        webring.indexOf(forgeDeployer),
+        1 
+      );
+
+      assertEq(
+        webring.myIndex(),
+        1
+      );
+    }
+
+    function testMemberInfo() public {
+      validMemberJoin();
+      (uint256 id, address addr, uint256 tokenId) = webring.memberInfo(forgeDeployer);
+      assertEq(id, 1);
+      assertEq(addr, validMemberAddress);
+      assertEq(tokenId, validMemberTokenId);
+    }
+
+    function contractAddressFor() public {
+      validMemberJoin();
+      assertEq(
+        webring.contractAddressFor(forgeDeployer),
+        validMemberAddress
+      );
+    }
+
 }
